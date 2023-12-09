224828fa3041b0b73a1d54b0d0c721637b7e6bc5
saadghazal <saad.m.ghazal@icloud.com>
2023-12-09 23:08:26 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+
+app.use((req,res,next)=>{
+    console.log('Middleware 1')
+    next()
+})
+
+app.use((req,res,next)=>{
+    console.log('Middleware 2')
+    res.send("<h1>Middleware 2 Response</h1>")
+
+})
+
+
+app.listen(3000)
