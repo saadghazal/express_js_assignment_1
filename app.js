fe1128fbb29e3023458f2fff88a988478021ecb3
saadghazal <saad.m.ghazal@icloud.com>
2023-12-09 23:17:03 +0300

    final commit

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const express = require('express')
 const app = express()
 
 
-app.use((req,res,next)=>{
-    console.log('Middleware 1')
-    next()
+// app.use((req,res,next)=>{
+//     console.log('Middleware 1')
+//     next()
+// })
+
+// app.use((req,res,next)=>{
+//     console.log('Middleware 2')
+//     res.send("<h1>Middleware 2 Response</h1>")
+
+// })
+
+app.use('/users',(req,res,next)=>{
+    res.send("<h1>Your Friends Page</h1>")
 })
 
-app.use((req,res,next)=>{
-    console.log('Middleware 2')
-    res.send("<h1>Middleware 2 Response</h1>")
-
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Home Page</h1>")
 })
+
+
+
+
 
 
 app.listen(3000)
